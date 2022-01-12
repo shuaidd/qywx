733e5f82679908cd4c85d5106e8ada9b78aeb6db
ddshuai <ddshuai@shinyway.com.cn>
2022-01-12 10:12:19 +0800

    优化细节，兼容spring boot 高版本

--- qywx-spring-boot-api/src/main/java/com/github/shuaidd/client/config/MultipartSupportConfig.java
+++ qywx-spring-boot-api/src/main/java/com/github/shuaidd/client/config/MultipartSupportConfig.java
@@ -2,11 +2,9 @@ package com.github.shuaidd.client.config;
 
 import com.github.shuaidd.client.decoder.WeChatMediaDecoder;
 import com.github.shuaidd.client.interceptor.WeChatInterceptor;
-import feign.Feign;
 import feign.codec.Decoder;
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -26,7 +24,7 @@ public class MultipartSupportConfig {
     }
 
     @Bean
-    public Feign.Builder builder(ApplicationContext applicationContext) {
-        return new Feign.Builder().requestInterceptor(new WeChatInterceptor());
+    public WeChatInterceptor weChatInterceptor() {
+        return new WeChatInterceptor();
     }
 }
